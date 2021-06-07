556cad3ce850e207d3fe4276b4c67348ab22ffe8
Jeffrey Huang <jeffreyhuangcpa@gmail.com>
2021-06-07 14:15:46 -0700

    Completed Linked List implementation

--- LinkedLists/ClassImplementation.js
+++ LinkedLists/ClassImplementation.js
@@ -84,6 +84,54 @@ class LinkedList {
   }
 
   removeElement(val) {
-    
+    let currentNode = this.head;
+    let prevNode;
+    let i = 0;
+    while (i < this.size) {
+      if (currentNode.val === val) {
+        if (i === 0) {
+          this.head = currentNode.next;
+        } else {
+          prevNode.next = currentNode.next;
+        }
+        this.size--;
+        return currentNode;
+      }
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+      i++;
+    }
+    return -1;
+  }
+
+  indexOf(val) {
+    let currentNode = this.head;
+    let i = 0;
+    while (currentNode !== null) {
+      if (currentNode.val === val) {
+        return i;
+      }
+      currentNode = currentNode.next;
+      i++;
+    }
+    return -1;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  size_of_list() {
+    console.log(this.size);
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let str = "";
+    while (currentNode !== null) {
+      str += currentNode.val + " ";
+      currentNode = currentNode.next;
+    }
+    console.log(str);
   }
 }
